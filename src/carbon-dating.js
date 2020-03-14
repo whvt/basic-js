38b92fc790e3de3444cb3d7b8f22d42fe1e4d7da
whvt <60581769+whvt@users.noreply.github.com>
2020-03-14 15:49:51 +0300

    done carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+LOG2 = 0.693; 
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+  let activity = parseFloat(sampleActivity);
+
+  if (activity > 0 && activity < MODERN_ACTIVITY) {
+    let ans = Math.log(MODERN_ACTIVITY / activity) / (LOG2 / HALF_LIFE_PERIOD);
+    
+    return Math.ceil(ans);
+  } 
+  return false;
 };
