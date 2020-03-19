9c2c88a2a74e20d32c3b5346d0f64163824cf1a1
whvt <60581769+whvt@users.noreply.github.com>
2020-03-19 18:12:18 +0300

    feat: complete dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+if (Array.isArray(members)) {
+    let ans = '';
+    members = members.filter(item => typeof item == 'string').map( item => item.trim().toUpperCase() ).sort();
+    for (let item of members){
+        ans += item[0];
+    }return ans;
+}else return false;
 };
