48faf86a04f940fcd055f736af6fb66db93a400d
whvt <60581769+whvt@users.noreply.github.com>
2020-03-19 17:33:03 +0300

    feat: complete count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,3 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  return  matrix.flat().filter(cat => cat == "^^").length; 
 };
