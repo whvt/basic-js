ff7b9e8a3a6bf3dbbceff0b99598f42e0544c618
whvt <60581769+whvt@users.noreply.github.com>
2020-03-22 18:08:23 +0300

    feat: complete transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,28 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)) {
+        throw new Error();
+    }
+    let  result = [];
+    for(let i = 0; i< arr.length; i++) {
+        if(arr[i] === '--double-next') {
+            if(i < arr.length - 1) {
+                result.push(arr[i + 1]);
+            }
+        } else if(arr[i] === '--double-prev') {
+            if(i > 0) {
+                result.push(arr[i - 1]);
+            }
+        } else if(arr[i] === '--discard-next') {
+            if(i < arr.length - 1) {
+                i++;
+            }
+        } else if(arr[i] === '--discard-prev') {
+            if(result.length > 0) {
+                result.pop();
+            }
+        } else {
+            result.push(arr[i]);
+        }
+    }
+    return result;
 };
